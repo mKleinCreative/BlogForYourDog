b7b4c5c3cd25caac80e9ad18decb7eb07d180b19
Michael Klein <miketheklein@gmail.com>
2018-06-21 09:11:36 -0700

    redux crud almost setup, just did delete

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-import { FETCH_POSTS, FETCH_POST } from '../actions'
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions'
 
 export default function (state = {}, action) {
   switch (action.type) {
@@ -8,6 +8,8 @@ export default function (state = {}, action) {
       return {...state, [action.payload.data.id]: action.payload.data}
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case DELETE_POST:
+      return ''
     default: 
       return state;
   }
