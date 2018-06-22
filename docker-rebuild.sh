a6a655f7b2d4e1b251accc83557c5249c50c9567
Dani Roxberry <dani@bitoriented.com>
2018-06-22 14:36:58 -0700

    [fix] node_modules volume stored and shared with host

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -2,4 +2,4 @@ docker-compose down && \
 docker volume rm blogforyourdog_pgdata && \
 docker volume rm blogforyourdog_frontend && \
 docker volume rm blogforyourdog_backend && \
-docker-compose up --build --quiet-pull --abort-on-container-exit --force-recreate
+docker-compose --verbose up --build --abort-on-container-exit --force-recreate
